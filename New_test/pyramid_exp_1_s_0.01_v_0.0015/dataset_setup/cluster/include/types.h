cf30c38622dc8a99f6c13f49354ab447376f9cd1
y8han <y8han@ucsd.edu>
2020-10-19 19:12:54 -0700

    update

--- New_test/pyramid_exp_1_s_0.01_v_0.0015/dataset_setup/cluster/include/types.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <cstddef>
-#include "stdint.h"
-
-
--- New_test/pyramid_exp_1_s_0.01_v_0.0015/dataset_setup/volume/vol_mesh.cpp
+++
@@ -1,9 +0,0 @@
-#include "mesh.h"
-#include "tetgen.h"
-#include "DeformativeObject3D.h"
-
-int main(int argc, char *argv[])
-{
-    Mesh test;
-    test.CreateVolumeMesh("../../test_pc/initial.ply","../..");
-}
