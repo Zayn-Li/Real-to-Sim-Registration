0350b82672a3fb3e773f8bec93ae7ebaf26f8dce
Zihan Li <zil027@eng.ucsd.edu>
2020-10-21 03:17:17 -0700

    videos for simulation and registration. Pyramaid results are terrible, so it's not included

---
+++ Videos/make-gifs.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import numpy as np
+import imageio
+import argparse
+import scipy.misc
+import matplotlib.pyplot
+import imageio
+from PIL import Image
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--src_dir', default='')
+parser.add_argument('--st_idx', type=int, default=0)
+parser.add_argument('--ed_idx', type=int, default=99)
+parser.add_argument('--height', type=int, default=926)
+parser.add_argument('--width', type=int, default=1448)
+
+args = parser.parse_args()
+st_x = 0
+ed_x = 80
+
+images = []
+
+for i in range(st_x, ed_x):
+
+    filename = os.path.join(args.src_dir, '02_L%02d.png' % i)
+    print(filename)
+    #img = imageio.imread(filename)
+	
+    img = Image.open(filename)
+    img = img.resize((args.width, args.height), Image.ANTIALIAS)
+    #img = cv2.resize(img, (args.width, args.height), interpolation=cv2.INTER_AREA)
+
+    images.append(img)
+
+imageio.mimsave(args.src_dir + '.gif', images, duration=1.0/10.0)
