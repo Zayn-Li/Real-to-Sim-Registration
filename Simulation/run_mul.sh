89f6497371ca878d7bc631208f50512154d326c3
y8han <y8han@ucsd.edu>
2020-10-22 20:14:15 -0700

    update

--- Simulation/run_mul.sh
+++ Simulation/run_mul.sh
@@ -1,18 +1,6 @@
 # !/usr/bin/env bash
 
 
-python3 PBD_3D_solver_NoRegis.py ./cube_exp_2
-
-python3 PBD_3D_solver_NoRegis.py ./lift_exp_4
-
-python3 PBD_3D_solver_NoRegis.py ./pyramid_exp_1
-
-python3 PBD_3D_solver_NoRegis.py ./sin_exp_1
-
-python3 PBD_3D_solver.py ./cube_exp_2
-
-python3 PBD_3D_solver.py ./lift_exp_4
-
 python3 PBD_3D_solver.py ./pyramid_exp_1
 
 python3 PBD_3D_solver.py ./sin_exp_1
